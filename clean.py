ab4381972b5f575dddbbae2bf65e920c394a21a9
Tom Karaffa <tdkaraffa@gmail.com>
2020-09-25 12:18:12 -0700

    second commit, added main.py

--- clean.py
+++ clean.py
@@ -1,26 +1,22 @@
-def select_dataframe(file_path: str):
+def select_dataframe(file_path: str, ignore_cols):
 	import pandas as pd
 	import os
 	import numpy as np
 	file_extension = os.path.splitext(file_path)
-
-	ignore_cols = ['Shop Number', 'Clients', 'Score', 'StoreNumber', 'StoreName', 'Address', 'City', 'State',
-				   'Zip', 'ShopDate', 'TimeIn', 'TimeOut', 'OverallComments']  # add
+	# add
 	# columns
 	# to ignore
 	# here
-
 	if file_extension == 'csv':
 		dataframe = pd.read_csv(file_path)
 		dataframe.drop(ignore_cols, inplace=True, axis=1)
 		return dataframe
 	elif file_extension == 'xlsx' or 'xls':
-		dataframe =  pd.read_excel(file_path)
+		dataframe = pd.read_excel(file_path)
 		dataframe.drop(ignore_cols, inplace=True, axis=1)
 		return dataframe
 	else:
 		return 'use excel or csv'
-
 
 
 def group_responses(series):
@@ -31,46 +27,41 @@ def group_responses(series):
 				if word in row[i]:
 					row[i] = word
 
-def cat_encoder(dataframe):
+
+def group_cols_by_type(dataframe, delimiters, open_text_cols):
+	s = (dataframe.dtypes == 'object')
+	cat_cols = list(s[s].index)
+	n = (dataframe.dtypes == 'number')
+	num_cols = list(n[n].index)
+	multi_cat_dataframe = dataframe[[col for col in cat_cols for d in delimiters if
+									 (dataframe[col].str.contains(d).any() and col not in open_text_cols)]].copy()
+	single_cat_dataframe = dataframe[[col for col in cat_cols if col not in multi_cat_dataframe.columns]].copy()
+	num_cat_dataframe = dataframe[num_cols]
+	open_text_dataframe = dataframe[open_text_cols]
+	return multi_cat_dataframe, single_cat_dataframe, num_cat_dataframe, open_text_dataframe
+
+
+def multi_cat_encoder(multi_cat_dataframe, delimiters):
 	from sklearn.preprocessing import MultiLabelBinarizer
 	import pandas as pd
-	s = (dataframe.dtypes=='object')
-	cat_cols = list(s[s].index)
-	delimiters = ['; \n']  # or any other character
-
-	open_text_cols = [
-	'REST4. Which business-owned, native platforms do you use to place orders from restaurants? (Please separate each with a comma.)',
-	'REST5. Which third-party platforms do you use to place orders from restaurants? (Please separate each with a '
-	'comma.)',
-	'G4. Which business-owned, native platforms do you use to place orders from grocery stores? (Please separate each with a comma.)',
-	'G5. Which third-party platforms do you use to place orders from grocery stores? (Please separate each with a '
-	'comma.)',
-	'RETAIL4. Which business-owned, native platforms do you use to place orders from retail establishments? (Please '
-	'separate each with a comma.)',
-	'RETAIL5. Which third-party platforms do you use to place orders from retail establishments? (Please separate '
-	'each with a comma.)',
-	'C4. Which business-owned, native platforms do you use to place orders from convenience stores? (Please separate '
-	'each with a comma.)',
-	'C5. Which third-party platforms do you use to place orders from convenience stores? (Please separate each with a comma.)'
-	]
-	multi_cat_cols = [col for col in cat_cols for d in delimiters if
-					  (dataframe[col].str.contains(d).any() and col not in open_text_cols)]
-	single_cat_cols = [col for col in cat_cols if col not in multi_cat_cols]
 	mlb = MultiLabelBinarizer()
-	multi_cat_df = df[multi_cat_cols].copy()
-	for col in multi_cat_cols:
-		# cleaning
-		multi_cat_df[col].fillna('Null', inplace=True) # temporarily replace nans with 'Null' to allow for encoding
+	from sklearn.pipeline import Pipeline
+	multi_cat_dataframe.fillna('null', inplace=True)  # temporarily replace nans with 'Null' to allow for encoding
+	for col in multi_cat_dataframe:
 		for d in delimiters:
-			split = multi_cat_df[col].str.split(d)
+			split = multi_cat_dataframe[col].str.split(d)
 			group_responses(split)
 			data = mlb.fit_transform(split)
 			col_names = [f'{col}: {a}' for a in mlb.classes_]
 			encoded = pd.DataFrame(data, columns=col_names)
-			encoded.mask(multi_cat_df[col]=='Null', inplace=True)
-			encoded.drop([col for col in encoded.columns if 'Null' in str(col)], inplace=True, axis=1)
-			multi_cat_df = pd.concat([multi_cat_df, encoded], axis=1)
-			multi_cat_df.drop(col, axis=1, inplace=True)
+			print(encoded.sum())
+			encoded.mask(multi_cat_dataframe[col] == 'null', inplace=True)
+			print(encoded.sum())
+			encoded.drop([col for col in encoded.columns if 'null' in str(col)], inplace=True, axis=1)
+			print(encoded.sum())
+			multi_cat_dataframe = pd.concat([multi_cat_dataframe, encoded], axis=1)
+			multi_cat_dataframe.drop(col, axis=1, inplace=True)
+	return multi_cat_dataframe
 
 	# start here
 	'''single_cat_cols = [col for col in dataframe.columns if col not in cat_cols_and_responses.keys()]
@@ -83,9 +74,4 @@ def cat_encoder(dataframe):
 	print(df)'''
 
 
-
-
 from sklearn.pipeline import Pipeline
-#main
-df = select_dataframe('Input/survey.xlsx')
-cat_encoder(df)
---
+++ main.py
@@ -0,0 +1,16 @@
+
+from clean import select_dataframe, group_cols_by_type, multi_cat_encoder
+# main
+
+
+columns_to_ignore = ['Shop Number', 'Clients', 'Score', 'StoreNumber', 'StoreName', 'Address', 'City', 'State', 'Zip',
+				   'ShopDate', 'TimeIn', 'TimeOut', 'OverallComments']
+
+dataframe = select_dataframe('Input/survey.xlsx', columns_to_ignore)
+
+delimiters = ['; \n'] # user-specified
+open_text_cols = ['Q50', 'Q51', 'Q91', 'Q92', 'Q132', 'Q133', 'Q173', 'Q174'] # user-specified
+
+multi_cat_df, single_cat_df, num_cat_df, open_text_df = group_cols_by_type(dataframe, delimiters, open_text_cols)
+
+multi_cat_encoder(multi_cat_df, delimiters)
