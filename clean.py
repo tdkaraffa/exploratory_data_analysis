f9bcf1fdce1e51226f5e2156f02045d06ed7fb19
Tom Karaffa <tdkaraffa@gmail.com>
2020-09-28 15:39:08 -0700

    added single category encoder

--- clean.py
+++ clean.py
@@ -23,9 +23,13 @@ def group_responses(series):
 	words_to_combine_on = ['N/A', 'Other']
 	for word in words_to_combine_on:
 		for row in series:
-			for i, e in enumerate(row):
-				if word in row[i]:
-					row[i] = word
+			if type(row) == list:
+				for i, e in enumerate(row):
+					if word in row[i]:
+						row[i] = word
+			elif type(row) == str:
+				if word in row:
+					row = word
 
 
 def group_cols_by_type(dataframe, delimiters, open_text_cols):
@@ -35,7 +39,8 @@ def group_cols_by_type(dataframe, delimiters, open_text_cols):
 	num_cols = list(n[n].index)
 	multi_cat_dataframe = dataframe[[col for col in cat_cols for d in delimiters if
 									 (dataframe[col].str.contains(d).any() and col not in open_text_cols)]].copy()
-	single_cat_dataframe = dataframe[[col for col in cat_cols if col not in multi_cat_dataframe.columns]].copy()
+	single_cat_dataframe = dataframe[
+		[col for col in cat_cols if col not in multi_cat_dataframe.columns and col not in open_text_cols]].copy()
 	num_cat_dataframe = dataframe[num_cols]
 	open_text_dataframe = dataframe[open_text_cols]
 	return multi_cat_dataframe, single_cat_dataframe, num_cat_dataframe, open_text_dataframe
@@ -45,33 +50,34 @@ def multi_cat_encoder(multi_cat_dataframe, delimiters):
 	from sklearn.preprocessing import MultiLabelBinarizer
 	import pandas as pd
 	mlb = MultiLabelBinarizer()
-	from sklearn.pipeline import Pipeline
 	multi_cat_dataframe.fillna('null', inplace=True)  # temporarily replace nans with 'Null' to allow for encoding
-	for col in multi_cat_dataframe:
+	for column in multi_cat_dataframe:
 		for d in delimiters:
-			split = multi_cat_dataframe[col].str.split(d)
+			split = multi_cat_dataframe[column].str.split(d)
 			group_responses(split)
 			data = mlb.fit_transform(split)
-			col_names = [f'{col}: {a}' for a in mlb.classes_]
-			encoded = pd.DataFrame(data, columns=col_names)
-			print(encoded.sum())
-			encoded.mask(multi_cat_dataframe[col] == 'null', inplace=True)
-			print(encoded.sum())
-			encoded.drop([col for col in encoded.columns if 'null' in str(col)], inplace=True, axis=1)
-			print(encoded.sum())
-			multi_cat_dataframe = pd.concat([multi_cat_dataframe, encoded], axis=1)
-			multi_cat_dataframe.drop(col, axis=1, inplace=True)
+			encoded_dataframe = pd.DataFrame(data, columns=[f'{column}: {a}' for a in mlb.classes_])
+			encoded_dataframe.index = multi_cat_dataframe.index
+			#encoded_dataframe.mask(multi_cat_dataframe[column] == 'null', inplace=True)
+			encoded_dataframe.drop([col for col in encoded_dataframe.columns if 'null' in str(col)], inplace=True,
+								   axis=1)
+			multi_cat_dataframe = pd.concat([multi_cat_dataframe, encoded_dataframe], axis=1)
+			multi_cat_dataframe.drop(column, axis=1, inplace=True)
 	return multi_cat_dataframe
 
-	# start here
-	'''single_cat_cols = [col for col in dataframe.columns if col not in cat_cols_and_responses.keys()]
+
+def one_hot_encoder(single_cat_dataframe):
 	from sklearn.preprocessing import OneHotEncoder
-	oh_encoder = OneHotEncoder(handle_unknown='ignore', sparse=True)
-	oh_cols = pd.DataFrame(oh_encoder.fit_transform(df[single_cat_cols]))
-	oh_cols.index = df[single_cat_cols].index
-	df.drop(single_cat_cols, inplace=True, axis=1)
-	pd.concat([df, oh_cols], axis=1)
-	print(df)'''
-
-
-from sklearn.pipeline import Pipeline
+	import pandas as pd
+	oh = OneHotEncoder(handle_unknown='ignore', sparse=False)
+	single_cat_dataframe.fillna('null', inplace=True)
+	for column in single_cat_dataframe:
+		group_responses(single_cat_dataframe[column])
+		data = oh.fit_transform(single_cat_dataframe[column].values.reshape(-1, 1)).astype(int)
+		encoded_dataframe = pd.DataFrame(data, columns=[f'{column}: {a}' for a in oh.categories_[0]])
+		encoded_dataframe.index = single_cat_dataframe.index
+		#encoded_dataframe.mask(single_cat_dataframe[column] == 'null', inplace=True)
+		encoded_dataframe.drop([col for col in encoded_dataframe.columns if 'null' in str(col)], inplace=True, axis=1)
+		single_cat_dataframe = pd.concat([single_cat_dataframe, encoded_dataframe], axis=1)
+		single_cat_dataframe.drop(column, axis=1, inplace=True)
+	return single_cat_dataframe
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from clean import select_dataframe, group_cols_by_type, multi_cat_encoder
+from clean import select_dataframe, group_cols_by_type, multi_cat_encoder, one_hot_encoder
 # main
 
 
@@ -14,3 +14,5 @@ open_text_cols = ['Q50', 'Q51', 'Q91', 'Q92', 'Q132', 'Q133', 'Q173', 'Q174'] # 
 multi_cat_df, single_cat_df, num_cat_df, open_text_df = group_cols_by_type(dataframe, delimiters, open_text_cols)
 
 multi_cat_encoder(multi_cat_df, delimiters)
+one_hot_encoder(single_cat_df)
+
