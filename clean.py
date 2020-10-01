71d9889537ae09d03c7ed4d5c74ea4cdb2d1a90e
Tom Karaffa <tdkaraffa@gmail.com>
2020-09-30 18:51:06 -0700

    combined encoder functions into one, cleaned up user-specified area

--- clean.py
+++ clean.py
@@ -15,8 +15,7 @@ def select_dataframe(file_path: str, ignore_cols):
 		return 'use excel or csv'
 
 
-def group_responses(series):
-	words_to_combine_on = ['N/A', 'Other']
+def group_responses(series, words_to_combine_on):  # user-specified
 	for word in words_to_combine_on:
 		for row in series:
 			if type(row) == list:
@@ -39,58 +38,38 @@ def group_cols_by_type(dataframe, delimiters, open_text_cols):
 	return multi_cat_cols, single_cat_cols, num_cols, open_text_cols
 
 
-def multi_cat_encoder(multi_cat_dataframe, delimiters):
-	from sklearn.preprocessing import MultiLabelBinarizer
+def cat_encoder(cat_dataframe, model, model_label_term, d=None, combine=None):  # combines the above two
+	# functions into one
+	# cat_data_frame: section of main dataframe to encode
+	# model: predetermined sklearn model
+	# model_label_term: model-specific parameter to obtain encoded labels
+	# d: optional list of delimiters
+	# combine: optional list of words with which to combine similar responses
 	import pandas as pd
 	import numpy as np
-	mlb = MultiLabelBinarizer()  # define model used to encode
-	working_dataframe = multi_cat_dataframe.fillna('null')  # temporarily replace NA with 'null' as needed for encoding
-	# initialize array & list for later appending
-	final_data = np.array(np.arange(len(working_dataframe))).reshape(-1, 1)
-	final_columns = ['index']
-	# iterate through columns, encoding data and collecting column names, and ensuring missing values are accounted for
-	for d in delimiters:
-		for column in working_dataframe:
-			split = working_dataframe[column].str.split(d)
-			group_responses(split)
-			data = mlb.fit_transform(split)
-			encoded_columns = [f'{column}: {a}' for a in mlb.classes_]
-			mask = working_dataframe[column] != 'null'
-			encoded_data = [row if m else [np.nan] * len(row) for row, m in zip(data, mask)]
-			# undo insertion of 'null' placeholders to keep missing values that are intentionally blank
-			try:
-				null_index = list(mlb.classes_).index('null')
-				encoded_data = np.delete(encoded_data, null_index, axis=1)
-				del encoded_columns[null_index]
-			except ValueError:
-				pass
-			# append newly encoded data to pre-existing arrays
-			final_data = np.append(final_data, encoded_data, axis=1)
-			final_columns = np.append(final_columns, encoded_columns)
-	# return all encoded data in a single dataframe
-	return pd.DataFrame(data=final_data, columns=final_columns)
-
-
-def one_hot_encoder(single_cat_dataframe):
-	from sklearn.preprocessing import OneHotEncoder
-	import pandas as pd
-	import numpy as np
-	oh = OneHotEncoder(handle_unknown='ignore', sparse=False)  # define model used to encode
-	working_dataframe = single_cat_dataframe.fillna('null')  # temporarily replace NA with 'null' as needed for
-	# encoding
-	# initialize array & list for later appending
-	final_data = np.array(np.arange(len(working_dataframe))).reshape(-1, 1)
+	working_dataframe = cat_dataframe.fillna('null')  # temporarily replace NA with 'null' as needed for encoding
+	final_data = np.array(np.arange(len(working_dataframe))).reshape(-1, 1)  # initialize array/list for later
+	# appending
 	final_columns = ['index']
 	# iterate through columns, encoding data and collecting column names, and ensuring missing values are accounted for
 	for column in working_dataframe:
-		group_responses(working_dataframe[column])
-		data = oh.fit_transform(working_dataframe[column].values.reshape(-1, 1)).astype(int)
-		encoded_columns = [f'{column}: {a}' for a in oh.categories_[0]]
+		if d:  # only execute this loop if delimiters are specified, only for multi-cat
+			for d in d:
+				split = working_dataframe[column].str.split(d)
+		else:  # otherwise, reshape data for model
+			split = working_dataframe[column].values.reshape(-1, 1)
+		if combine:
+			group_responses(split, combine)  # groups similar responses, like those containing "Other" or "N/A"
+		data = model.fit_transform(split)
+		labels = getattr(model, model_label_term)  # get categories/classes from model to label encoded data
+		if len(labels) == 1:  # handles single cat columns, because of how categories_ method of OneHotEncoder returns
+			labels = labels[0]
+		encoded_columns = [f'{column}: {a}' for a in labels]
 		mask = working_dataframe[column] != 'null'
 		encoded_data = [row if m else [np.nan] * len(row) for row, m in zip(data, mask)]
 		# undo insertion of 'null' placeholders to keep missing values that are intentionally blank
 		try:
-			null_index = list(oh.categories_[0]).index('null')
+			null_index = list(labels).index('null')
 			encoded_data = np.delete(encoded_data, null_index, axis=1)
 			del encoded_columns[null_index]
 		except ValueError:
--- main.py
+++ main.py
@@ -1,19 +1,28 @@
+from clean import select_dataframe, group_cols_by_type, cat_encoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MultiLabelBinarizer
 
-from clean import select_dataframe, group_cols_by_type, multi_cat_encoder, one_hot_encoder
-# main
-
-
+# -------------------------------------------------------------------------------------------------------------------- #
+# USER-SPECIFIED INPUTS
+file = 'Input/survey.xlsx'
 columns_to_ignore = ['Shop Number', 'Clients', 'Score', 'StoreNumber', 'StoreName', 'Address', 'City', 'State', 'Zip',
-				   'ShopDate', 'TimeIn', 'TimeOut', 'OverallComments']
+					 'ShopDate', 'TimeIn', 'TimeOut', 'OverallComments']
+delimiters = ['; \n']  # user-specified
+open_text_cols = ['Q50', 'Q51', 'Q91', 'Q92', 'Q132', 'Q133', 'Q173', 'Q174']  # user-specified
+words_to_combine_on = ['N/A', 'Other']  # user-specified
+# -------------------------------------------------------------------------------------------------------------------- #
 
-dataframe = select_dataframe('Input/survey.xlsx', columns_to_ignore)
+dataframe = select_dataframe(file, columns_to_ignore)
+multi_cat_cols, single_cat_cols, num_cat_cols, open_text_cols = group_cols_by_type(dataframe, delimiters,
+																				   open_text_cols)
+multi_cat_model = MultiLabelBinarizer()  # define model used to encode
+multi_cat_label_term = 'classes_'  # specify the parameter to obtain encoded labels
+single_cat_model = OneHotEncoder(handle_unknown='ignore', sparse=False)  # define model used to encode
+single_cat_label_term = 'categories_'  # specify the parameter to obtain encoded labels
 
-delimiters = ['; \n'] # user-specified
-open_text_cols = ['Q50', 'Q51', 'Q91', 'Q92', 'Q132', 'Q133', 'Q173', 'Q174'] # user-specified
-
-multi_cat_cols, single_cat_cols, num_cat_cols, open_text_cols = group_cols_by_type(dataframe, delimiters, open_text_cols)
-
-multi_cat_encoded_df = multi_cat_encoder(dataframe[multi_cat_cols], delimiters)
-single_cat_encoded_df = one_hot_encoder(dataframe[single_cat_cols])
+multi_cat_encoded_df = cat_encoder(dataframe[multi_cat_cols], multi_cat_model, multi_cat_label_term, d=delimiters,
+								   combine=words_to_combine_on)
+single_cat_encoded_df = cat_encoder(dataframe[single_cat_cols], single_cat_model, single_cat_label_term,
+									combine=words_to_combine_on)
 num_cat_df = dataframe[num_cat_cols].copy()
 open_text_df = dataframe[open_text_cols]
