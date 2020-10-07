90ff43cd7e7ddd3bd141a251745874c64e9cebb3
Tom Karaffa <50383991+tdkaraffa@users.noreply.github.com>
2020-10-06 20:48:46 -0700

    Update visualizations.py

--- visualizations.py
+++ visualizations.py
@@ -7,7 +7,6 @@ single_cat_data = pickle.load(open("single_cat_data.p", "rb"))
 multi_cat_data = pickle.load(open("multi_cat_data.p", "rb"))
 num_data = pickle.load(open("num_data.p", "rb"))
 demo_df = pickle.load(open("demos_data.p", "rb"))
-# end delete block
 # ---------------------------------------------------------#
 
 
